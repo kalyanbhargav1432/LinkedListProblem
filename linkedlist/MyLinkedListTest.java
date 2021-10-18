d05fbe02ca7d7c312c1dbe1a458555d85ee5962b
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-10-18 17:27:35 +0530

    Added:Functionality to inserting 30 between 56 and 70

--- linkedlist/MyLinkedListTest.java
+++ linkedlist/MyLinkedListTest.java
@@ -33,4 +33,19 @@ public class MyLinkedListTest {
 				&& myLinkedList.tail.equals(myThirdNode);
 		Assert.assertTrue(result);
 	}
+
+	@Test
+	public void given3NumbersWhenInsertingSecondInBetweenShouldPassLinkedListResult() {
+		MyNodes<Integer> myFirstNode = new MyNodes<>(56);
+		MyNodes<Integer> mySecondNode = new MyNodes<>(30);
+		MyNodes<Integer> myThirdNode = new MyNodes<>(70);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.add(myFirstNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.insert(myFirstNode, mySecondNode);
+		myLinkedList.printMyNodes();
+		boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.equals(mySecondNode)
+				&& myLinkedList.tail.equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
 }
